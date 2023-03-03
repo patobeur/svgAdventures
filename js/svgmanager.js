ed5e9f3eb5e0a5471be675f5332a3863bf1c17cc
Patobeur <patobeur41@gmail.com>
2023-03-03 07:50:30 +0100

    update A

--- js/svgmanager.js
+++ js/svgmanager.js
@@ -11,21 +11,34 @@ class SvgManager {
 	}
 	createMap() {
 		Con.addMessage('Creating Map...')
-		this.SvgMap = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-		this.SvgMap.setAttribute("viewBox", `-${this.margetemporaireLeft}  -${this.margetemporaireTop} ${this.map.datas.width + (this.margetemporaireTop + this.margetemporaireLeft)} ${this.map.datas.height + (this.margetemporaireTop + this.margetemporaireLeft)}`);
-		this.SvgMap.setAttribute("width", this.map.datas.width + (this.margetemporaireLeft + this.margetemporaireRigth) + 'px');
-		this.SvgMap.setAttribute("height", this.map.datas.height + (this.margetemporaireTop + this.margetemporaireBottom) + 'px');
-		this.SvgMap.setAttribute("class", 'map');
+		let elementdatas = {
+			tag: 'svg',
+			viewBox: `-${this.margetemporaireLeft}  -${this.margetemporaireTop} ${this.map.datas.width + (this.margetemporaireTop + this.margetemporaireLeft)} ${this.map.datas.height + (this.margetemporaireTop + this.margetemporaireLeft)}`,
+			class: 'map',
+			width: this.map.datas.width + (this.margetemporaireLeft + this.margetemporaireRigth) + 'px',
+			height: this.map.datas.height + (this.margetemporaireTop + this.margetemporaireBottom) + 'px',
+			// fill: 'rgba(0,0,255,1)',
+		};
+		this.SvgMap = Svg.getSvgElement(elementdatas)
 		this.SvgMap.setAttribute("xml:space", 'preserve');
 		this.SvgMap.setAttribute("enable-background:new", `0 0 ${this.map.datas.width} ${this.map.datas.height}`);
 
 		this.refreshSvgMap()
 		this.addSvgMapToDom()
 
-		this.addRules()
 	}
 	addCadre() {
-		let cadre = this.getRect(-this.margetemporaireLeft, -this.margetemporaireTop, this.map.datas.width, this.map.datas.width, 'rgba(0,0,255,.1)')
+		let elementdatas = {
+			tag: 'rect',
+			x: 0,
+			y: 0,
+			width: this.map.datas.width,
+			height: this.map.datas.height,
+			fill: 'rgba(0,0,255,.1)',
+		};
+
+		let cadre = Svg.getSvgElement(elementdatas)
+		// let cadre = this.getRect(-this.margetemporaireLeft, -this.margetemporaireTop, this.map.datas.width, this.map.datas.height, 'rgba(0,0,255,.1)')
 		this.SvgMap.prepend(cadre)
 
 	}
@@ -45,7 +58,7 @@ class SvgManager {
 		}
 
 	}
-	getSvgElement(elementdatas) {
+	getSvgElement(elementdatas, source = false) {
 		let element = document.createElementNS("http://www.w3.org/2000/svg", elementdatas.tag);
 		for (const attrib in elementdatas) {
 			if (Object.hasOwnProperty.call(elementdatas, attrib)) {
@@ -59,18 +72,11 @@ class SvgManager {
 		window.onresize = () => { this.refreshSvgMap() }
 	}
 	refreshSvgMap() {
-		let top = 0
-		let left = 0
-		if (Players.player) {
-			top = (Players.player.datas.top + (Players.player.datas.height / 2))
-			left = (Players.player.datas.left + (Players.player.datas.width / 2))
-			// to do 
-			// si circle rayon
-			// si rect top left ??? 
-		}
-		else {
-			top = (this.map.datas.height / 2)
-			left = (this.map.datas.width / 2)
+		let top = (this.map.datas.height / 2)
+		let left = (this.map.datas.width / 2)
+		if (Players && Players.player) {
+			top = Players.player.datas.top
+			left = Players.player.datas.left
 		}
 		this.SvgMap.style.top = Math.floor(((window.innerHeight / 2) - top)) + 'px'
 		this.SvgMap.style.left = Math.floor(((window.innerWidth / 2) - left)) + 'px'
@@ -91,24 +97,13 @@ class SvgManager {
 			case 'rect':
 				elementdatas = {
 					tag: Players.player.shape.tag,
-					x: Players.player.datas.left,
-					y: Players.player.datas.top,
+					x: Players.player.datas.left - (Players.player.datas.width / 2),
+					y: Players.player.datas.top - (Players.player.datas.height / 2),
 					width: Players.player.datas.width,
 					height: Players.player.datas.height,
 					fill: Players.player.shape.color,
 				}
 				break;
-			// case 'polygon':
-			// 	this.SvgPlayer = this.getPolygon(
-			// 		Players.player.datas.left,
-			// 		Players.player.datas.top,
-			// 		Players.player.datas.width,
-			// 		Players.player.datas.height,
-			// 		3,
-			// 		Players.player.shape.color,
-			// 		'player'
-			// 	)
-			// 	break;
 			default:
 				break;
 		}
@@ -116,75 +111,44 @@ class SvgManager {
 		this.SvgMap.prepend(this.SvgPlayer);
 		this.refreshSvgMap()
 	}
-	getCircle(cx, cy, r, c, u) {
-		let shape = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-		if (u) {
-			shape.setAttribute("cx", (Number(cx) + Number(r)));
-			shape.setAttribute("cy", (Number(cy) + Number(r)));
-		}
-		shape.setAttribute("r", r);
-		shape.setAttribute("fill", c);
-		return shape
-	}
-	getRect(x, y, w, h, c, u) {
-		let shape = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-		if (u) {
-			shape.setAttribute("x", Number(x));//+ (Number(w) / 2)));
-			shape.setAttribute("y", Number(y));//+ (Number(h) / 2)));
-		}
-		shape.setAttribute("width", w);
-		shape.setAttribute("height", h);
-		shape.setAttribute("fill", c);
-		return shape
-	}
-	getPolygon(cx, cy, w, h, s, c, u) {
-		const surfaceRect = w * h;
-		const apothem = w / (2 * Math.tan(Math.PI / s));
-		const perimeter = s * w;
-		const side = perimeter / s;
-		let points = "";
-		for (let i = 0; i < s; i++) {
-			const angle = 2 * Math.PI * (i / s);
-			const x = w / 2 + apothem * Math.sin(angle);
-			const y = h / 2 - apothem * Math.cos(angle);
-			points += `${x},${y} `;
-		}
-		let shape = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-		shape.setAttribute("points", points);
-		shape.setAttribute("fill", c);
-		if (u) {
-			shape.setAttribute("cx", cx);
-			shape.setAttribute("cy", cy);
-		}
-		return shape;
-	}
-	getTriangleFromCircle(cx, cy, r, c) {
-		const surfaceCircle = Math.PI * Math.pow(r, 2);
-		const base = Math.sqrt((4 * surfaceCircle) / Math.sqrt(3));
-		const height = Math.sqrt((3 * surfaceCircle) / (2 * base));
-		const x1 = r;
-		const y1 = 0;
-		const x2 = x1 - base / 2;
-		const y2 = height;
-		const x3 = x1 + base / 2;
-		const y3 = height;
-		let shape = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-		shape.setAttribute("points", `${x1},${y1} ${x2},${y2} ${x3},${y3}`);
-		shape.setAttribute("fill", c);
-		shape.setAttribute("cx", cx);
-		shape.setAttribute("cy", cy);
-		return shape;
-	}
-	getLine(x1, y1, x2, y2, color, width = 1) {
-		let shape = document.createElementNS("http://www.w3.org/2000/svg", "line");
-		shape.setAttribute("x1", x1);
-		shape.setAttribute("y1", y1);
-		shape.setAttribute("x2", x2);
-		shape.setAttribute("y2", y2);
-		shape.setAttribute("stroke", color);
-		shape.setAttribute("stroke-width", width);
-		return ele
-	}
+	// getPolygon(cx, cy, w, h, s, c, u) {
+	// 	const surfaceRect = w * h;
+	// 	const apothem = w / (2 * Math.tan(Math.PI / s));
+	// 	const perimeter = s * w;
+	// 	const side = perimeter / s;
+	// 	let points = "";
+	// 	for (let i = 0; i < s; i++) {
+	// 		const angle = 2 * Math.PI * (i / s);
+	// 		const x = w / 2 + apothem * Math.sin(angle);
+	// 		const y = h / 2 - apothem * Math.cos(angle);
+	// 		points += `${x},${y} `;
+	// 	}
+	// 	let shape = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+	// 	shape.setAttribute("points", points);
+	// 	shape.setAttribute("fill", c);
+	// 	if (u) {
+	// 		shape.setAttribute("cx", cx);
+	// 		shape.setAttribute("cy", cy);
+	// 	}
+	// 	return shape;
+	// }
+	// getTriangleFromCircle(cx, cy, r, c) {
+	// 	const surfaceCircle = Math.PI * Math.pow(r, 2);
+	// 	const base = Math.sqrt((4 * surfaceCircle) / Math.sqrt(3));
+	// 	const height = Math.sqrt((3 * surfaceCircle) / (2 * base));
+	// 	const x1 = r;
+	// 	const y1 = 0;
+	// 	const x2 = x1 - base / 2;
+	// 	const y2 = height;
+	// 	const x3 = x1 + base / 2;
+	// 	const y3 = height;
+	// 	let shape = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+	// 	shape.setAttribute("points", `${x1},${y1} ${x2},${y2} ${x3},${y3}`);
+	// 	shape.setAttribute("fill", c);
+	// 	shape.setAttribute("x", cx);
+	// 	shape.setAttribute("y", cy);
+	// 	return shape;
+	// }
 	addRessources(numSquares = false) {
 		// Générer un nombre aléatoire de carrés verts
 		if (!numSquares || numSquares < 1) { let numSquares = Math.floor(Math.random() * 10) + 1; } // entre 1 et 10 carrés
